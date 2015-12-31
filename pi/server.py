9522de1a612945646f6dd6ccc70b7010ec2f8b02
Luca Keidel <info@lucakeidel.de>
2015-12-31 02:13:34 +0100

    Refactored sever and Arduino README

--- pi/server.py
+++ pi/server.py
@@ -4,14 +4,18 @@ import paho.mqtt.client as mqtt
 import re
 import serial
 
-
 SERIAL_PORT = '/dev/ttyUSB0'
 
-topicPattern = re.compile("/home/sockets/group(\d+)/socket(\d+)")
+BROKER_HOST = 'localhost'
+BROKER_PORT = 1883
+USERNAME = 'username'
+PASSWORD = 'password'
+
+topicPattern = re.compile('/home/sockets/group(\d+)/socket(\d+)')
 ser = serial.Serial(SERIAL_PORT)
 
 def on_connect(client, obj, flags, rc):
-    client.subscribe("/home/sockets/#", 0)
+    client.subscribe('/home/sockets/#', 0)
 
 def on_message(client, obj, msg):
 
@@ -37,20 +41,14 @@ def on_message(client, obj, msg):
 
         if(ord(resp[0]) == 0):
             client.publish('/home/socket/response/group'+str(group)+'/socket'+str(socket), 'OK '+message)
-            print "OK"
         else:
             client.publish('/home/socket/response/group'+str(group)+'/socket'+str(socket), 'ERR '+message+' '+str(ord(resp[0])))
-            print "ERR"
-            # ERR <errorno>
-
-    print("Message: ")
-    print(pmsg)
 
 client = mqtt.Client()
 client.on_message = on_message
 client.on_connect = on_connect
-client.username_pw_set("username", "password")
+client.username_pw_set(USERNAME, PASSWORD)
 
-client.connect("localhost", 1883)
+client.connect(BROKER_HOST, BROKER_PORT)
 
 client.loop_forever()
